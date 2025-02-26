8079df18318cd7bbf4d7adf14d1ec76b66eb1920
Alf <42708476+alfset@users.noreply.github.com>
2025-02-27 02:40:12 +0700

    Create auth.ts

---
+++ auth.ts
@@ -0,0 +1,5 @@
+export const connect = {
+    twitterAuthUrl: 'https://bakcend-surabaya-blockchain-aliance.vercel.app/connect/twitter',
+    discordAuthUrl: 'https://bakcend-surabaya-blockchain-aliance.vercel.app/connect/discord',
+    telegramAuthUrl: 'https://bakcend-surabaya-blockchain-aliance.vercel.app/connect/telegram',
+  };
