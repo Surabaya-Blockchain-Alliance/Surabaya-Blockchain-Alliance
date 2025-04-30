e6dea63b63c8b7f9f332973836074c8ac51943a8
alfset <alfset5@gmail.com>
2025-04-30 22:06:04 +0700

    add wallet signer for proof of wallet owner

--- pages/api/save-profile.js
+++ pages/api/save-profile.js
@@ -1,5 +1,6 @@
-import { db } from '../../config'; 
 import { doc, getDocs, setDoc, query, where } from 'firebase/firestore';
+import { db } from '../../config'; 
+
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
